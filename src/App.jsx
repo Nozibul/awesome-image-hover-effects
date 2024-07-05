cb8372ffbeb57d40f72f51e50fe49c83dc8cd751
Nozibul <nozibulislamspi@gmail.com>
2024-07-05 16:30:39 +0600

    9 awesome image effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,246 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import Image1 from './components/image1/Image1'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <h2>Lily</h2> */}
+        <div className="grid">
+          <figure className="effect-lily">
+            <img
+              src="https://tympanus.net/Development/HoverEffectIdeas/img/12.jpg"
+              alt="img12"
+            />
+            <figcaption>
+              <div>
+                <h2>
+                  Nice <span>Lily</span>
+                </h2>
+                <p>Lily likes to play with crayons and pencils</p>
+              </div>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-lily">
+            <img
+              src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg"
+              alt="img1"
+            />
+            <figcaption>
+              <div>
+                <h2>
+                  Nice <span>Lily</span>
+                </h2>
+                <p>Lily likes to play with crayons and pencils</p>
+              </div>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div>
+
+        {/* <h2>Sadie</h2> */}
+        <div className="grid">
+          <figure className="effect-sadie">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/2.jpg" alt="img02" />
+            <figcaption>
+              <h2>
+                Holy <span>Sadie</span>
+              </h2>
+              <p>
+                Sadie never took her eyes off me. <br />
+                She had a dark soul.
+              </p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-sadie">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/14.jpg" alt="img14" />
+            <figcaption>
+              <h2>
+                Holy <span>Sadie</span>
+              </h2>
+              <p>
+                Sadie never took her eyes off me. <br />
+                She had a dark soul.
+              </p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div>
+
+        {/* <h2>Layla</h2> */}
+        <div className="grid">
+          <figure className="effect-layla">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/6.jpg" alt="img06" />
+            <figcaption>
+              <h2>
+                Crazy <span>Layla</span>
+              </h2>
+              <p>When Layla appears, she brings an eternal summer along.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-layla">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/3.jpg" alt="img03" />
+            <figcaption>
+              <h2>
+                Crazy <span>Layla</span>
+              </h2>
+              <p>When Layla appears, she brings an eternal summer along.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div>
+
+        {/* <h2>Bubba</h2>  */}
+        <div className="grid">
+          <figure className="effect-bubba">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/17.jpg" alt="img17" />
+            <figcaption>
+              <h2>
+                Gorgeous <span>Bubba</span>
+              </h2>
+              <p>Bubba likes to appear out of thin air.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-bubba">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/18.jpg" alt="img18" />
+            <figcaption>
+              <h2>
+                Gorgeous <span>Bubba</span>
+              </h2>
+              <p>Bubba likes to appear out of thin air.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div> 
+
+        {/* <h2>Oscar</h2> */}
+        <div className="grid">
+          <figure className="effect-oscar">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/9.jpg" alt="img09" />
+            <figcaption>
+              <h2>
+                Warm <span>Oscar</span>
+              </h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-oscar">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/10.jpg" alt="img10" />
+            <figcaption>
+              <h2>
+                Warm <span>Oscar</span>
+              </h2>
+              <p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div> 
+
+        {/* <h2>Dexter</h2> */}
+        <div className="grid">
+          <figure className="effect-dexter">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/21.jpg" alt="img21" />
+            <figcaption>
+              <h2>
+                Strange <span>Dexter</span>
+              </h2>
+              <p>Dexter had his own strange way. You could watch him training ants.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-dexter">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/22.jpg" alt="img22" />
+            <figcaption>
+              <h2>
+                Strange <span>Dexter</span>
+              </h2>
+              <p>Dexter had his own strange way. You could watch him training ants.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div> 
+
+        {/* <h2>Sarah</h2> */}
+        <div className="grid">
+          <figure className="effect-sarah">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/23.jpg" alt="img23" />
+            <figcaption>
+              <h2>
+                Free <span>Sarah</span>
+              </h2>
+              <p>Sarah likes to watch clouds. quite depressed.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-sarah">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/24.jpg" alt="img24" />
+            <figcaption>
+              <h2>
+                Free <span>Sarah</span>
+              </h2>
+              <p>Sarah likes to watch clouds.  quite depressed.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div> 
+
+        {/* <h2>Milo</h2> */}
+				<div className="grid">
+					<figure className="effect-milo">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/11.jpg" alt="img11"/>
+						<figcaption>
+							<h2>Faithful <span>Milo</span></h2>
+							<p>Milo went to the woods. He took a fun ride and never came back.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+					<figure className="effect-milo">
+						<img src="https://tympanus.net/Development/HoverEffectIdeas/img/3.jpg" alt="img03"/>
+						<figcaption>
+							<h2>Faithful <span>Milo</span></h2>
+							<p>Milo went to the woods. He took a fun ride and never came back.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div> 
+
+        {/* <h2>Chico</h2> */}
+        <div className="grid">
+          <figure className="effect-chico">
+            <img
+              src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg"
+              alt="img15"
+            />
+            <figcaption>
+              <h2>
+                Silly <span>Chico</span>
+              </h2>
+              <p>Chico main fear was missing the morning bus.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure className="effect-chico">
+            <img
+              src="https://tympanus.net/Development/HoverEffectIdeas/img/4.jpg"
+              alt="img04"
+            />
+            <figcaption>
+              <h2>
+                Silly <span>Chico</span>
+              </h2>
+              <p>Chico main fear was missing the morning bus.</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
